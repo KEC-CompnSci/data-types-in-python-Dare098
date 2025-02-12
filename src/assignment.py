4ecbd2e618413862a78ce3dba12506f68ed871b4
Dare098 <27daret@retsdstudent.ca>
2025-02-12 21:02:55 +0000

    thank you for the help

--- src/assignment.py
+++ src/assignment.py
@@ -5,7 +5,14 @@
 num_str_1 = "42"
 num_str_2 = "13"
 num_str_3 = "7"
-word_1 = "robots"
-word_2 = "built"
+word_1 = " robots "
+word_2 = "built "
 word_3 = "today"
-word_4 = "were"
+word_4 = "were "
+first = int(num_str_1)
+second = int(num_str_2)
+calculation = first + second
+calculation_as_string = str(calculation)
+actual_number = int(calculation)
+sentence = word_1 + word_4 + word_2 + word_3
+sentence = calculation_as_string + sentence
